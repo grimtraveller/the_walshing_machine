eab9f3316ad2d46732ef8dece4dda70d73b437cb
Ian Clarkson <clarksonian@gmail.com>
2012-03-03 14:56:09 -0500

    Now have a single processing call for both float and double types that is templated. Outputting a 440Hz tone for testing purposes.

--- walshing_machine.cpp
+++ walshing_machine.cpp
@@ -1,6 +1,33 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include "walshing_machine.h"
 
 void WalshingMachine::processReplacing(float** inputs, float** outputs, VstInt32 sampleFrames)
 {
+  process<float>(inputs, outputs, sampleFrames);
+}
+
+void WalshingMachine::processDoubleReplacing(double** inputs, double** outputs, VstInt32 sampleFrames)
+{
+  process<double>(inputs, outputs, sampleFrames);
+}
+
+template <typename T> 
+void WalshingMachine::process(T** inputs, T** outputs, VstInt32 sampleFrames)
+{
+  // get our time info
+  VstTimeInfo* time_info = getTimeInfo(NULL);
+
+  // don't do anything if we're not playing...
+  if (!(time_info->flags & kVstTransportPlaying))
+    return;
+
+  // set output to a 440Hz wave
+  for (int i = 0; i < kNumOutputs; ++i)
+    for (int j = 0; j < sampleFrames; ++j)
+      outputs[i][j] = sin(2 * M_PI * (time_info->samplePos + j) / time_info->sampleRate * 440);
+
   return;
 }
+
--- walshing_machine.h
+++ walshing_machine.h
@@ -4,10 +4,20 @@ class WalshingMachine : public AudioEffectX
 {
 public:
   WalshingMachine(audioMasterCallback audioMaster, VstInt32 numPrograms, VstInt32 numParams) 
-    : AudioEffectX(audioMaster, numPrograms, numParams) {}
+    : AudioEffectX(audioMaster, numPrograms, numParams) 
+  {
+	  setNumInputs(kNumInputs);   // stereo in
+	  setNumOutputs(kNumOutputs); // stereo out
+	  setUniqueID('ThWM');        // you must change this for other plug-ins!
+  	canProcessReplacing();      // supports replacing mode
+    canDoubleReplacing();       // supports double replacing mode
+  }
 
-	///< Process 32 bit (single precision) floats (always in a resume state)
+  // Process 32 bit (single precision) floats (always in a resume state)
   virtual void processReplacing(float** inputs, float** outputs, VstInt32 sampleFrames); 
+  
+  // Process 64 bit (double precision) floats (always in a resume state)
+  virtual void processDoubleReplacing(double** inputs, double** outputs, VstInt32 sampleFrames);
 
   enum Params
   {
@@ -19,4 +29,12 @@ public:
   {
     kNumPrograms
   };
+
+private:
+
+  template <typename T> 
+  void process(T** inputs, T** outputs, VstInt32 sampleFrames);
+
+  static const int kNumInputs  = 2;
+  static const int kNumOutputs = 2;
 };
